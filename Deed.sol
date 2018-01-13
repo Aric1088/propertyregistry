d435d6019f77a97c080918190008326f90ba2bf8
Aric1088 <ariczhuang@gmail.com>
2018-01-13 14:38:44 -0500

    Update Deed.sol

--- Deed.sol
+++ Deed.sol
@@ -10,7 +10,6 @@ contract PropertyRegistry{
 
     mapping (address => Owner) owners;
     address[] public ownerAccts;
-    string[] public properties;
 
     function setOwner( address _address, string _name, string _propaddress, string _price) public {
         var owner = owners[_address];
@@ -18,11 +17,7 @@ contract PropertyRegistry{
         owner.name = _name;
         owner.propaddress = _propaddress;
         owner.price = _price;
-        properties.push(_propaddress) -1;
         ownerAccts.push(_address) -1;
-    }
-    function getProperties() view public returns (string[]){
-      return properties;
     }
     function getOwners() view public returns (address[]){
         return ownerAccts;
